f13eba8ef68b2d8693ead7ce689918a2d74fad4c
Bruno Pierini <brunomiguelpie@gmail.com>
2020-10-26 14:56:02 -0300

    indent changes

--- application/views/senhas/mostra.php
+++ application/views/senhas/mostra.php
@@ -1,15 +1,7 @@
-<html>
-<head>
-	<link rel="stylesheet" href="<?= base_url("css/bootstrap.css") ?>">
-</head>
-<body>
-<div class="container">
+
 	Usuario: <?= html_escape($senha["passwords_user"])?><br/>
 	Senha: <?= html_escape($senha["passwords_password"])?><br/>
 	Descricao: <?= auto_typography(html_escape($senha["description"]))?>
 	Data de insercao: <?= html_escape($senha["data_de_insercao"])?></br>
 
 	<?= anchor('senhas/index', 'Voltar', array("class" => "btn btn-primary")) ?>
-</div>
-</body>
-</html>
